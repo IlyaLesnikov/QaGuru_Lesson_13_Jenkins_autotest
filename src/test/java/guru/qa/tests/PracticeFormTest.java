49bebee83b57a0a1b81872a3a477708de3642c79
i.lesnikov <i.lesnikov@bars-group.ru>
2023-12-10 21:06:20 +0300

    Addesd tasks for Jenkins

--- src/test/java/guru/qa/tests/PracticeFormTest.java
+++ src/test/java/guru/qa/tests/PracticeFormTest.java
@@ -2,20 +2,23 @@ package guru.qa.tests;
 
 import guru.qa.pages.components.CheckResultComponent;
 import guru.qa.utils.TestData;
+import io.qameta.allure.Description;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import guru.qa.pages.PracticeFormPage;
 
 import static io.qameta.allure.Allure.step;
-
+@DisplayName("Модуль 'Форма практики'")
 public class PracticeFormTest extends BaseTest {
     PracticeFormPage practiceFormPage = new PracticeFormPage();
     CheckResultComponent checkResultComponent = new CheckResultComponent();
     TestData data = new TestData();
     @Test
     @Tag("SMOKE")
+    @DisplayName("Отправка веб-форма со всеми полями заполеннными валидными значениями")
     public void allFieldsAreFilledInTest(){
-        step("Заполнение веб-формы валидными значениями и отправка её", () -> {
+        step("Заполнение и отправка веб-формы с валидными значениями", () -> {
             practiceFormPage.openWebForm()
                     .closedBanner()
                     .setValueFirstName(data.firstName)
@@ -31,14 +34,15 @@ public class PracticeFormTest extends BaseTest {
                     .setValueSelectStateAndCity(data.state, data.city)
                     .buttonClick();
         });
-        step("Проверка результатов заполнения формы", () -> {
+        step("Проверка результатов заполнения веб-формы", () -> {
             checkResultComponent.AssertResultComponent(data.label, data.values);
         });
     }
     @Test
     @Tag("WEB")
+    @DisplayName("Отправка веб-форма с заполеннными обязательными полями валидными значениями")
     public void onlyRequiredFieldsAreFilledInTest(){
-        step("Заполнение веб-формы валидными значениями и отправка её", () -> {
+        step("Заполнение и отправка веб-формы с валидными значениями", () -> {
             practiceFormPage.openWebForm()
                     .closedBanner()
                     .setValueFirstName(data.firstName)
@@ -48,7 +52,7 @@ public class PracticeFormTest extends BaseTest {
                     .setValueMobile(data.phone)
                     .buttonClick();
         });
-        step("Проверка результатов заполнения формы", () -> {
+        step("Проверка результатов заполнения веб-формы", () -> {
             checkResultComponent.checkResult("Student Name", data.firstName + " " + data.lastName)
                     .checkResult("Student Email", data.email)
                     .checkResult("Gender", data.gender)
@@ -57,8 +61,9 @@ public class PracticeFormTest extends BaseTest {
     }
     @Test
     @Tag("WEB")
+    @DisplayName("Отправка веб-формы с одним не заполненным обязательным полем")
     public void oneIsNotFilledInNecessarilyTest() {
-        step("Заполнение веб-формы валидными значениями и отправка её", () -> {
+        step("Заполнение и отправка веб-формы с валидными значениями", () -> {
             practiceFormPage.openWebForm()
                     .closedBanner()
                     .setValueFirstName(data.firstName)
@@ -67,7 +72,7 @@ public class PracticeFormTest extends BaseTest {
                     .setValueMobile(data.phone)
                     .buttonClick();
         });
-        step("Проверка результатов заполнения формы", () -> {
+        step("Проверка результатов заполнения веб-формы", () -> {
             checkResultComponent.checkResultElement("Practice Form");
         });
     }
